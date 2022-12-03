63f7413209378ea6be9105ce6d4b82c0c2280bcc
Kennysaruni <kenny.saruni@student.moringaschool.com>
2022-12-03 09:25:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(
+      (found) => found.toLowerCase() === name.toLowerCase()
+    );
+  }
+function fuzzyMatch(drivers,name){
+    return drivers.filter(
+        (found) => found.toLowerCase().indexOf(name.toLowerCase()) === 0
+    );
+}
+function matchName(drivers, name) {
+    return drivers.filter((newsource) => newsource.name === name);
+  }
